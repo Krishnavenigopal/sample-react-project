d1e08aba7458b895b5c085739ec1c8ba245af1ef
Krishnaveni venugopal <krishnaveniv90@gmail.com>
2022-11-11 02:13:18 +0530

    connect home page to main app

--- react-application/src/components/home.js
+++ react-application/src/components/home.js
@@ -1,14 +1,14 @@
-import { useHistory, useLocation, useParams, useRouteMatch } from "react-router-dom"
+import { useNavigate, useLocation, useParams, useMatch } from "react-router-dom"
 
 export default function Home() {
     //Access history object with useHistory
-    const history = useHistory();
+    const history = useNavigate();
 
     //Access the location object with useLocation
     const location = useLocation();
 
     //Access the match object with useRouteMacth
-    const match = useRouteMatch();
+    const match = useMatch();
 
     //Extract the url parameters with useParams
     const params = useParams();
